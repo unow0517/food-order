e5ab4a9758f6698e6f882cabc5e5d381f2a63a50
unow0517 <unow1992@gmail.com>
2020-12-29 10:22:35 +0100

    change to remote sql

--- config/constants.php
+++ config/constants.php
@@ -4,13 +4,21 @@
     //here all about data connection with our website
     
     //Create Constants to Stroe Non Repeating Values
-    define('SITEURL','http://localhost/food-order/');
-    define('LOCALHOST','localhost');
-    define('DB_USERNANE','root');
-    define('DB_PASSWORD','');
-    define('DB_NAME','food-order');
+    // define('SITEURL','http://localhost/food-order/');
+    // define('LOCALHOST','localhost');
+    // define('DB_USERNANE','root');
+    // define('DB_PASSWORD','');
+    // define('DB_NAME','food-order');
 
-    $conn = mysqli_connect(LOCALHOST, DB_USERNANE, DB_PASSWORD) or die(mysqli_error()); //Database Connection
+    //Remote Sql
+    define('SITEURL','remotesql.com');
+    define('HOST','remotesql.com');
+    define('DB_USERNANE','fv5Ta1LEFP');
+    define('DB_PASSWORD','y0ruXjvav6');
+    define('DB_NAME','fv5Ta1LEFP');
+
+
+    $conn = mysqli_connect(HOST, DB_USERNANE, DB_PASSWORD) or die(mysqli_error()); //Database Connection
       
     $db_select = mysqli_select_db($conn, DB_NAME) or di(mysqli_error()); //Selecting Database
 ?>
